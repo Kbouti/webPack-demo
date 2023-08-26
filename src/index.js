9c0e6a4f1dc8aee5c2f2541d2d9ba7feacd72a19
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-25 18:32:18 -0700

    Edit package.json file

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@
 //5. npm install webpack webpack-cli --save-dev
 //6. make src folder, and append child index.js, index.html
 //7. boiler plate index and test js command
+//8. in package.json file: remove main: index.js, replace with:     "private": true,
 
 
-
-
+console.log(`I am here in the console`);
 
 function component() {
     const element = document.createElement('div)');
 
     //Lodash, currently included via a script, is required for this line to work:
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     //^That's their code and comment, I'm not sure if lodash is somehow needed to concact the strings but I'm using the simplified code below
     
-    element.innerHTML = `If all has gone well you're reading this on the screen. `;
+    // element.innerHTML = `If all has gone well you're reading this on the screen. `;
 
     return element;
 }
