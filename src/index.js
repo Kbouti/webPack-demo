db0a57ff9b4fa941bed1164338eb4cc4883a7807
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-26 08:18:03 -0700

    Update comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 //Steps before modifying index.js:
-//1. create git repo
-//2. navigate to the folder you want your repo to live in
-//3. clone repo (git clone (ssh))
+//1. Create git repo
+//2. Navigate to the folder you want your repo to live in
+//3. Clone repo (git clone (ssh))
 //4. npm init -y
 //5. npm install webpack webpack-cli --save-dev
-//6. make src folder, and append child index.js, index.html
-//7. boiler plate index and test js command
-//8. in package.json file: remove main: index.js, replace with:     "private": true,
-//9. create dist folder, put index inside it (could have done it this way from the start?)
+//6. Make src folder, and append child index.js, index.html
+//7. Boiler plate index and test js command
+//8. In package.json file: remove main: index.js, replace with:     "private": true,
+//9. Create dist folder, put index inside it (could have done it this way from the start?)
 //10. npm install --save lodash
-//11. add import _ from 'lodash' to src/index.js
-//12. adjust script tag in html and run "npx webpack"
+//11. Add import _ from 'lodash' to src/index.js
+//12. Adjust script tag in html and run "npx webpack" in terminal. At this point you need to run npx webpack to make any changes active on screen. 
 
 
 
@@ -26,7 +26,7 @@ function component() {
     //Lodash, currently included via a script, is required for this line to work:
     //now lodash is imported by this script
     
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     //^That's their code and comment, I'm not sure if lodash is somehow needed to concact the strings but I'm using the simplified code below
     
